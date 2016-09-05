4d66dc25c32216c5306d17bc9e8717cb2a6deae5
bharbich <bharbich@gmail.com>
2016-09-04 22:27:10 -0700

    adding newtonsoft plugin

--- FlipperProject/Assets/Test.cs
+++ FlipperProject/Assets/Test.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Newtonsoft.Json;
 
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        JsonConvert.SerializeObject(1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
