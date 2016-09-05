efba8f12fefd6da75bfc2a240e828b18290ec4b8
aauVadim <vadimosipov45@gmail.com>
2016-09-04 22:03:29 -0700

    adding server

---
+++ Server/FlipperServer/FlipperServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+using Photon;
+
+namespace FlipperServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject go = new GameObject();
+            Console.WriteLine(go.ToString());
+            Console.ReadLine();
+        }
+    }
+}
